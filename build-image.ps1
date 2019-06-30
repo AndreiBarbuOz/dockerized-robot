b118aec3198d04a475b1f8d6eb349fc14a756e92
Andrei Barbu <andrei.barbu@uipath.com>
2019-07-01 09:46:52 +1000

    test new build script

--- build-image.ps1
+++ build-image.ps1
@@ -1,7 +1,10 @@
+param([string]$file_name)
+
+$ecr_repo_name = Get-Content $file_name -Raw
 $region = (ConvertFrom-Json (Invoke-WebRequest -Uri http://169.254.169.254/latest/dynamic/instance-identity/document -UseBasicParsing).Content).region
 Invoke-Expression -Command (Get-ECRLoginCommand -Region $region).Command
 
-$ecr_repo_name = Get-CFNExport | Where-Object {$_.Name -eq 'docker-build:ecr-uri'} | Select -ExpandProperty 'Value'
+#$ecr_repo_name = Get-CFNExport | Where-Object {$_.Name -eq 'docker-build:ecr-uri'} | Select -ExpandProperty 'Value'
 
 Write-Host $ecr_repo_name
 
